143c10af4f058055d220a817b47d26beb6d3ca7c
Bryan Tran <tran_b1@denison.edu>
2019-12-06 17:22:39 -0800

    Website server initiated

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.use(express.static('./public'));
+
+// app.get('/', (request, response)=>{
+//     response.send('Hello from the back side');
+// })
+
+app.listen(PORT, ()=>{
+    console.log(`listening on ${PORT}`);
+})
