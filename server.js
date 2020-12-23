aacacb656f9a3043c61f6ce567cd26067566b18d
trannon1 <tran_b1@denison.edu>
2020-12-22 16:34:55 -0800

    Example

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
-'use strict'
+'use strict';
 
-const express = require('express');
-const app = express();
-require('dotenv').config();
+const express = require('express')
+require('dotenv').config()
+const app = express()
+require('ejs')
 
 const PORT = process.env.PORT;
 
 app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded ({ extended: true, }));
+app.get('/', getHome);
+app.get('/portfolio', getInterest);
+app.get('/about', getAbout);
+app.get('/contact', getContact);
 
-// app.get('/', (request, response)=>{
-//     response.send('Hello from the back side');
-// })
+function getHome(request, response){
+    response.render('home');
+  }
+
+function getInterest(request, response){
+    response.render('portfolio');
+}
+
+function getAbout(request, response){
+    response.render('about');
+}
+
+function getContact(request, response){
+    response.render('contact');
+}
+
+app.get('*', (request, response) =>{
+    response.status(404).send("Page not found");
+})
 
 app.listen(PORT, ()=>{
     console.log(`listening on ${PORT}`);
